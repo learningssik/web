f2b0f2540387af6349661b57c13796db82ef7f6a
Your Name Here <learningssik@gmail.com>
2022-05-12 13:21:28 +0900

    Refactor: js file을 통한 코드 간소화

---
+++ color.js
@@ -0,0 +1,41 @@
+
+
+  var Links = {
+    SetColor:function(color){
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while(i < alist.length){
+        alist[i].style.color = color;
+        i = i + 1;
+      }
+    }
+  }
+
+  var Body = {
+    setColor:function(color){
+      document.querySelector('body').style.color=color;
+    },
+    setBackgroundColor:function(color){
+      document.querySelector('body').style.backgroundColor=color;
+    }
+  }
+
+
+
+  function NightDayHandler(self){
+    var target = document.querySelector('body');
+    if (self.value === 'night'){
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value = 'day';
+      Links.SetColor('powderblue');
+
+
+    } else {
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value = 'night';
+
+      Links.SetColor('blue');
+      }
+  }
